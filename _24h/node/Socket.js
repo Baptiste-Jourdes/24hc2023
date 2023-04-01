3a18a132acd9618ad17bce075fed75a4fd620edc
Baptiste <baptiste.jourdes.etu@univ-lemans.fr>
2023-04-01 18:25:56 +0200

    jhgf

--- _24h/node/Socket.js
+++ _24h/node/Socket.js
@@ -1,3 +1,4 @@
+const  dgram=require('dgram');
 const mdns = require('mdns-js');
 const serviceType = '_carnode._udp.local';
 const name = 'CarNode-Simu6._arduino._tcp.local';
@@ -18,10 +19,49 @@ browser.on('update', function(data) {
 
     // Connectez-vous au service
     const address = data.addresses[0];
-    const port = data.port;
-    const socket = require('net').connect({host: address, port: port}, () => {
-      console.log('Connected to service');
+    //const server = dgram.createSocket('udp4');
+    const port = 4211;
+    //const socket = require('net').connect({host: address, port: port}, () => {
+
+    const dgram = require('dgram');
+
+// Créer une socket UDP
+    const socket = dgram.createSocket('udp4');
+
+    socket.bind(port)
+
+
+    socket.on("listening", function() {
+      this.setBroadcast(true);
+      this.setMulticastTTL(128);
+      this.addMembership('224.0.0.251');
+      console.log('Multicast listening . . . ')
+    })
+
+
+    socket.on('message', (msg, remote) => {
+      console.log(msg.toString().trim())
+    })
+
+    const client = dgram.createSocket('udp4');
+ //Envoyer un message UDP
+     for(let  i=0;i<9;i++)
+     {
+       const message = Buffer.from('Hello, world!');
+       client.send(message, 0, message.length, port, data.addresses[0], (err) => {
+         if (err) {
+           console.error(`Erreur lors de l'envoi du message : ${err}`);
+         } else {
+           console.log('Message envoyé avec succès.');
+         }
+       });
+     }
+
+    //server.on('message');
+    //console.log('Connected to service');
+
+
       // Utilisez le socket pour communiquer avec le service
-    });
+    //});
   }
 });
