5c2ca90d477ae60d7f55a100f0757804e0230118
ClementJamelot <clement.jamelot.etu@univ-lemans.fr>
2023-04-01 14:16:35 +0200

    Clément

---
+++ Clément/Socket.js
@@ -0,0 +1,22 @@
+const mdns = require('mdns-js');
+const serviceType = '_http._tcp';
+
+// Recherchez les services mDNS
+const browser = mdns.createBrowser(serviceType);
+browser.on('ready', function() {
+  console.log('mdns browser ready');
+  browser.discover();
+});
+browser.on('update', function(data) {
+  if (data.type === serviceType) {
+    console.log('Service up: ', data.fullname, data.addresses);
+
+    // Connectez-vous au service
+    const address = data.addresses[0];
+    const port = data.port;
+    const socket = require('net').connect({host: address, port: port}, () => {
+      console.log('Connected to service');
+      // Utilisez le socket pour communiquer avec le service
+    });
+  }
+});
