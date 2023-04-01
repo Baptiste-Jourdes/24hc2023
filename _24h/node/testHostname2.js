4368faff50428c867a82d0659a1eb1e888550ad3
Baptiste <baptiste.jourdes.etu@univ-lemans.fr>
2023-04-01 19:33:42 +0200

    jhgf

---
+++ _24h/node/testHostname2.js
@@ -0,0 +1,9 @@
+var bonjour = require('bonjour')()
+
+// advertise an HTTP server on port 3000
+bonjour.publish({ name: 'CarNode-Simu6.local', type: 'arduino', port: 23 })
+
+// browse for all http services
+bonjour.find({ type: 'arduino' }, function (service) {
+    console.log('Found an HTTP server:', service)
+})
