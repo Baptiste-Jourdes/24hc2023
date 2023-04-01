46c223bac595c605d68f34dd83b8ce704bfff9d0
Florian-Bauge <Florian.Bauge.Etu@univ-lemans.fr>
2023-04-02 00:52:37 +0200

    socket

--- Clément/Socket2.js
+++ Clément/Socket2.js
@@ -6,14 +6,14 @@ var nom=""
 const instance = new mdns.Bonjour()
 const dgram = require('dgram');
 const socket = dgram.createSocket('udp4');
+
 // advertise an arduino server on port 8266
 instance.publish({name: 'My Web Server',type: 'arduino',  port: 8266 })
 
 // browse for all carnode services with udp protocol
-let timer =setTimeout(affichage,3000)
+let timer =setTimeout(affichage,5000)
 instance.find({type: 'carnode',protocol:'udp' }, function (service) {
     mytab.push([service.name,service.addresses,service.port])
-    
     
 })
 
@@ -54,13 +54,20 @@ function Server()
  })
 
     var trame = {RSSI: [3,5], IR: [9,2], SIMU: [11, 7] ,HEADLIGHTS: [17, 3], COLOR: [3,7], BATTERY: [11,5], IMU: [15, 13] ,PILOTS: [28, 6]};
+    var hasFirst = false;
 
     socket.on('message', (msg, remote) => {
-        //console.log(msg)
-        if(msg.length == 20) {
-            //console.log(msg.slice(trame.RSSI[0], trame.RSSI[0]+trame.RSSI[1]).toString('hex'));
-            if(msg.slice(trame.RSSI[0], trame.RSSI[0]+trame.RSSI[1]).toString('hex') == '01c2ffffff') {
-                console.log(msg.slice(trame.COLOR[0], trame.COLOR[0] + trame.COLOR[1]))
+        if(remote.address == mytab[nom][1][0]) {
+            console.log(remote.address)
+            console.log('Voiture 6')
+            console.log(msg)
+
+            if (msg.length == 31) {
+                //console.log(msg.slice(trame.RSSI[0], trame.RSSI[0]+trame.RSSI[1]).toString('hex'));
+                //if (msg.slice(trame.RSSI[0], trame.RSSI[0] + trame.RSSI[1]).toString('hex') == '01c2ffffff') {
+
+                    console.log(msg.slice(trame.COLOR[0], trame.COLOR[0] + trame.COLOR[1]))
+                //}
             }
         }
 
@@ -71,7 +78,7 @@ function Server()
 //Envoyer un message UDP
   for(let  i=0;i<9;i++)
   {
-    let frame = new Uint8Array([0x43, 0x49, 0x53, 0x02 , 0x00 , 0x00 ,0x00 ,0x00,0x00,0x00, 0x33, 0xff, 0xff, 0xff]);
+    let frame = new Uint8Array([0x43, 0x49, 0x53, 0x02 , 0x00 , 0x00 ,0x00 ,0x00,0x00,0x00, 0x33, 0x00, 0xff, 0x00 ]);
     const message = Buffer.from('Hello, world!');
     console.log(mytab[nom][1][0])
     client.send(frame, 0, frame.length, mytab[nom][2], mytab[nom][1][0], (err) => {
